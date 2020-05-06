8d67ea9f15d53ee3f0df59187c964e6c26ecc9dc
takeshi0311 <fabio.1993.2019@gmail.com>
2020-05-06 22:15:38 +0900

    修正６

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -280,25 +280,6 @@ class LinebotController < ApplicationController
                 text: push
               }
               client.reply_message(event['replyToken'], message_text)
-            # per06to12 = doc.elements[xpath + 'info/rainfallchance/period[2]l'].text
-            # per12to18 = doc.elements[xpath + 'info/rainfallchance/period[3]l'].text
-            # per18to24 = doc.elements[xpath + 'info/rainfallchance/period[4]l'].text
-            # if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-            #   word =
-            #     ["雨だけど元気出していこうね！",
-            #      "雨に負けずファイト！！",
-            #      "雨だけどああたの明るさでみんなを元気にしてあげて(^^)"].sample
-            #   push =
-            #     "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
-            # else
-            #   word =
-            #     ["天気もいいから一駅歩いてみるのはどう？(^^)",
-            #      "今日会う人のいいところを見つけて是非その人に教えてあげて(^^)",
-            #      "素晴らしい一日になりますように(^^)",
-            #      "雨が降っちゃったらごめんね(><)"].sample
-            #   push =
-            #     "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
-            # end
           end
           # テキスト以外（画像等）のメッセージが送られた場合
         else
