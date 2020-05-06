948e80f3cafd4b3fd052b9a395c3d5582bf91aac
takeshi0311 <fabio.1993.2019@gmail.com>
2020-05-06 19:27:39 +0900

    天気予報_条件分岐修正６

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -27,17 +27,17 @@ class LinebotController < ApplicationController
           xml  = open( url ).read.toutf8
           doc = REXML::Document.new(xml)
           xpath = 'weatherforecast/pref/area[2]/'
-          weather        = doc.elements[xpath + 'info[2]/weather'].text
           website        = "https://www.jma.go.jp/jp/week/329.html"
-          morning        = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text.to_i
-          moonning       = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text.to_i
-          evening        = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text.to_i
-          maxtemperature = doc.elements[xpath + 'info[2]/temperature/range[1]'].text.to_i
-          mintemperature = doc.elements[xpath + 'info[2]/temperature/range[2]'].text.to_i
           weatherchange = ["一時", "時々", "後", "のち", "後一時", "後時々"]
           case input
             # 「明日」or「あした」というワードが含まれる場合
           when /.*(明日|あした).*/
+            weather        = doc.elements[xpath + 'info[2]/weather'].text
+            morning        = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text.to_i
+            moonning       = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text.to_i
+            evening        = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text.to_i
+            maxtemperature = doc.elements[xpath + 'info[2]/temperature/range[1]'].text.to_i
+            mintemperature = doc.elements[xpath + 'info[2]/temperature/range[2]'].text.to_i
             if weather == "晴れ"
               imgurl        = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS7HQA6bcOaK5FlAjC8-iEOuM7Ad6KgUuSAlm_R_R_0HoRpimk9&usqp=CAU"
               iconurl1      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
@@ -45,8 +45,6 @@ class LinebotController < ApplicationController
               iconurl3      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
               bottomcomment = "明日は晴れです！　　　　　いい天気です。　　　　　　明日も頑張っていきましょう！！"
               weathertext   = "明日は#{weather}です。"
-              # push =
-              #   "明日の天気だよね。\n明日は雨が降りそうだよ(>_<)\n今のところ降水確率はこんな感じだよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
             elsif weather == "くもり"
               imgurl        = "https://www.imgstyle.info/files/thumb_0e4e0045d7ba40177a86b889d700c29c.jpg"
               iconurl1      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
@@ -61,7 +59,6 @@ class LinebotController < ApplicationController
               iconurl3      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
               bottomcomment = "明日は雨が降りそうです。　傘を忘れないで下さい！　　明日も一日頑張りましょう！"
               weathertext   = "明日は#{weather}です。"
-            # elsif weatherchange.any? { |t| weather.include?(t) }
             else
               matchstring = weatherchange.find { |t| weather.include?(t) }
               weathercombination = weather.split(matchstring)
@@ -108,19 +105,84 @@ class LinebotController < ApplicationController
                     bottomcomment = "明日は#{weather}です。傘を忘れずに持って行って下さい！後に雨は上がるでしょう！明日も1日頑張って行きましょう！"
                     weathertext   = "明日は#{weather}です。"
                 end
-              # push =
-              #   "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
             end
           when /.*(明後日|あさって).*/
-            per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
-            per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
-            per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
-            if per06to12.to_i >= min_per || per12to18.to_i >= min_per || per18to24.to_i >= min_per
-              push =
-                "明後日の天気だよね。\n何かあるのかな？\n明後日は雨が降りそう…\n当日の朝に雨が降りそうだったら教えるからね！"
+            weather        = doc.elements[xpath + 'info[3]/weather'].text
+            morning        = doc.elements[xpath + 'info[3]/rainfallchance/period[2]'].text.to_i
+            moonning       = doc.elements[xpath + 'info[3]/rainfallchance/period[3]'].text.to_i
+            evening        = doc.elements[xpath + 'info[3]/rainfallchance/period[4]'].text.to_i
+            maxtemperature = doc.elements[xpath + 'info[3]/temperature/range[1]'].text.to_i
+            mintemperature = doc.elements[xpath + 'info[3]/temperature/range[2]'].text.to_i
+            # per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
+            # per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
+            # per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
+            if weather == "晴れ"
+              imgurl        = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS7HQA6bcOaK5FlAjC8-iEOuM7Ad6KgUuSAlm_R_R_0HoRpimk9&usqp=CAU"
+              iconurl1      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
+              iconurl2      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
+              iconurl3      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
+              bottomcomment = "明日は晴れです！　　　　　いい天気です。　　　　　　明日も頑張っていきましょう！！"
+              weathertext   = "明日は#{weather}です。"
+            elsif weather == "くもり"
+              imgurl        = "https://www.imgstyle.info/files/thumb_0e4e0045d7ba40177a86b889d700c29c.jpg"
+              iconurl1      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
+              iconurl2      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
+              iconurl3      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
+              bottomcomment = "明日はくもりです。雨が降る可能性もありますので折りたたみ傘があると安心です！　明日も一日頑張りましょう！"
+              weathertext   = "明日は#{weather}です。"
+            elsif weather == "雨"
+              imgurl        = "https://pure-life.biz/wp-content/uploads/2019/04/1113098.png"
+              iconurl1      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
+              iconurl2      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
+              iconurl3      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
+              bottomcomment = "明日は雨が降りそうです。　傘を忘れないで下さい！　　明日も一日頑張りましょう！"
+              weathertext   = "明日は#{weather}です。"
             else
-              push =
-                "明後日の天気？\n気が早いねー！何かあるのかな。\n明後日は雨は降らない予定だよ(^^)\nまた当日の朝の最新の天気予報で雨が降りそうだったら教えるからね！"
+              matchstring = weatherchange.find { |t| weather.include?(t) }
+              weathercombination = weather.split(matchstring)
+                if weathercombination[0] == "晴れ" && weathercombination[1] == "くもり"
+                    imgurl        = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThoh8dTaGHeBQbzhj5FhkRuD6aBade-0cvppsH75fMYYKPRGb_&usqp=CAU"
+                    iconurl1      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
+                    iconurl2      = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXKbLJX8i5eWVaivnyU0ZOc7I1-mrOwm6MFS2N52qVGsJ73kNN&usqp=CAU"
+                    iconurl3      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
+                    bottomcomment = "明日は#{weather}です。　明日も1日頑張って行きましょう！"
+                    weathertext   = "明日は#{weather}です。"
+                elsif weathercombination[0] == "晴れ" && weathercombination[1] == "雨"
+                    imgurl        = "https://weather.jldn-info.jp/Weather/img/weekly-weather-icon-012-sun-rny.png"
+                    iconurl1      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
+                    iconurl2      = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXKbLJX8i5eWVaivnyU0ZOc7I1-mrOwm6MFS2N52qVGsJ73kNN&usqp=CAU"
+                    iconurl3      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
+                    bottomcomment = "明日は#{weather}です。　　明日は雨が降りそうだから傘を忘れないでね！　　　　　明日も1日頑張って行きましょう！"
+                    weathertext   = "明日は#{weather}です。"
+                elsif weathercombination[0] == "くもり" && weathercombination[1] == "晴れ"
+                    imgurl        = "https://weathernews.auone.jp/au/img/telop/210.png"
+                    iconurl1      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
+                    iconurl2      = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXKbLJX8i5eWVaivnyU0ZOc7I1-mrOwm6MFS2N52qVGsJ73kNN&usqp=CAU"
+                    iconurl3      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
+                    bottomcomment = "明日は#{weather}です。　明日は午後から晴れる予報です！明日も1日頑張って行きましょう！"
+                    weathertext   = "明日は#{weather}です。"
+                elsif weathercombination[0] == "くもり" && weathercombination[1] == "雨"
+                    imgurl        = "https://weather.jldn-info.jp/Weather/img/weekly-weather-icon-022-cld-rny.png"
+                    iconurl1      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
+                    iconurl2      = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXKbLJX8i5eWVaivnyU0ZOc7I1-mrOwm6MFS2N52qVGsJ73kNN&usqp=CAU"
+                    iconurl3      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
+                    bottomcomment = "明日は#{weather}です。　明日は雨が降りそうなので傘を忘れないで下さい！　　　明日も1日頑張って行きましょう！"
+                    weathertext   = "明日は#{weather}です。"
+                elsif weathercombination[0] == "雨" && weathercombination[1] == "晴れ"
+                    imgurl        = "https://pbs.twimg.com/profile_images/877491396931764226/sWoT1gWx_400x400.jpg"
+                    iconurl1      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
+                    iconurl2      = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXKbLJX8i5eWVaivnyU0ZOc7I1-mrOwm6MFS2N52qVGsJ73kNN&usqp=CAU"
+                    iconurl3      = "https://www.photolibrary.jp/mhd4/img252/450-20120524224023194686.jpg"
+                    bottomcomment = "明日は#{weather}です。傘を忘れずに持って行って下さい！後に雨は上がるでしょう！明日も1日頑張って行きましょう！"
+                    weathertext   = "明日は#{weather}です。"
+                elsif weathercombination[0] == "雨" && weathercombination[1] == "くもり"
+                    imgurl        = "https://weathernews.auone.jp/au/img/telop/313.png"
+                    iconurl1      = "https://www.cjs.ne.jp/umeda/blog/wp-content/uploads/2018/05/ame.png"
+                    iconurl2      = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTXKbLJX8i5eWVaivnyU0ZOc7I1-mrOwm6MFS2N52qVGsJ73kNN&usqp=CAU"
+                    iconurl3      = "https://i.pinimg.com/originals/5c/cc/7b/5ccc7b3eac3830853c4c55e95124ae95.png"
+                    bottomcomment = "明日は#{weather}です。傘を忘れずに持って行って下さい！後に雨は上がるでしょう！明日も1日頑張って行きましょう！"
+                    weathertext   = "明日は#{weather}です。"
+                end
             end
           when /.*(かわいい|可愛い|カワイイ|きれい|綺麗|キレイ|素敵|ステキ|すてき|面白い|おもしろい|ありがと|すごい|スゴイ|スゴい|好き|頑張|がんば|ガンバ).*/
             push =
